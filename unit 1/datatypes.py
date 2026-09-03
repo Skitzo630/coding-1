0ab0a4d9d59ec8d5a0377721461c151101db55ea
Skitzo630 <eliasthesavage800@gmail.com>
2026-09-03 14:20:37 +0000

    variable class work

---
+++ unit 1/datatypes.py
@@ -0,0 +1,36 @@
+#STRING-characters that are wrapped within quotes. 
+# characters can be letters, numbers, and symbols. 
+  #you can use both single and double qutation.
+# mark for strings
+ 
+  ''elias richardson"
+    
+" 123 main street" 
+ 
+ "this cost 10.00$"
+   
+#the hashtag is a maker that makes anyhting written
+ # behind it invisible to the computer
+  
+#intgers- datatypes that are just whole numbers or 
+#without a decinal point we do not add any special symbol 
+ 
+ 10
+  20 
+  2000000
+ 
+ # boolean- a data type that can only be either true or false 
+ #we do not use quotation marks for boolean vaules. the computer
+ # understand these words as something unique 
+  
+false
+  true 
+
+
+    
+     
+applecount = 30 
+studentcount = 25 
+travleinsurance = add 
+we choose the gold membership its better than the bronze membership and the sliver membership
+diffrentdrinks = for a protein shake - the price is 15:99, for the milkshake  - the price is 12:99 ,and for your water -its 5:99
---
+++ unit 1/variableactivity.py
@@ -0,0 +1,4 @@
+#variable- a container that stores data.you can name yourcontainer almost anything you wante 
+'name= elias richardson'
+# when we want to make a variable we make up an word us equal sign and assign it to the data
+#whenever the computer sees our varieable word it will alway use data 
