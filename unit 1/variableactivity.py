8c498be1b5c26f5de87ce9679fed1f2a6989b05a
Skitzo630 <eliasthesavage800@gmail.com>
2026-09-04 14:38:32 +0000

    claswork

--- unit 1/variableactivity.py
+++ unit 1/variableactivity.py
@@ -2,3 +2,18 @@
 'name= elias richardson'
 # when we want to make a variable we make up an word us equal sign and assign it to the data
 #whenever the computer sees our varieable word it will alway use data 
+#variable naming conventions
+ #we use nameing conventions because we cannot have spaces in
+# variable names 
+ 
+# camel case - when creating variable names with multiple words, 
+#every new word begins  with capital letter 
+#the reason why its called camel case is bc evry nw word 
+#represents a hump. 
+ 
+
+  
+# snake case - when creating a variable using snake case 
+# we use a underscore symbol to space out each new word 
+# the reason why its called snake  case is bc the underscore 
+#represents a snake on the groud new_word_
