832433822839be3da579f37ddfcc43478e28845f
Skitzo630 <eliasthesavage800@gmail.com>
2026-09-09 14:10:20 +0000

    classwork

--- unit 1/datatypes.py
+++ unit 1/datatypes.py
@@ -3,11 +3,11 @@
   #you can use both single and double qutation.
 # mark for strings
  
-  ''elias richardson"
+''elias richardson"
     
 " 123 main street" 
  
- "this cost 10.00$"
+"this cost 10.00$"
    
 #the hashtag is a maker that makes anyhting written
  # behind it invisible to the computer
@@ -15,9 +15,9 @@
 #intgers- datatypes that are just whole numbers or 
 #without a decinal point we do not add any special symbol 
  
- 10
-  20 
-  2000000
+     10
+     20 
+     2000000
  
  # boolean- a data type that can only be either true or false 
  #we do not use quotation marks for boolean vaules. the computer
