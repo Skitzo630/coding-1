561b7d2645879e97aed0930e865005b2477c6fef
Skitzo630 <eliasthesavage800@gmail.com>
2026-09-16 14:30:57 +0000

    class w ork

--- unit 1/operatorsactivity.py
+++ unit 1/operatorsactivity.py
@@ -1,4 +1,32 @@
 print(200 ==  100)# false 
 print (17 > 18) # false
 print("coding 1" == "coding1")# false  
-print ("0" !=  0) # true
+print ("0" !=  0) # true 
+
+
+
+
+
+
+
+
+
+
+
+book = 10.99 
+tablet = 399.99 
+print (10.99 + 399.99) 
+
+cart =98.87 
+amounttoget=100.00 
+print(amounttoget > cart ) 
+
+shoes = 200.00 
+Tax= 0.07 
+print(Tax* shoes) 
+
+gpa = 86
+recommendation =True 
+print (gpa + recommendation) 
+
+
