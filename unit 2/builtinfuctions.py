37e1f90b99f2578a415c153ee2cfe4c566c79388
Skitzo630 <eliasthesavage800@gmail.com>
2026-09-22 14:33:19 +0000

    class work

---
+++ unit 2/builtinfuctions.py
@@ -0,0 +1,47 @@
+# function - simply put : a code block of 
+# instructions for computer to follow. 
+
+# built- in fuction - a code block of 
+# instructions for a computer to follow 
+#that was already written for us  
+# PRE-WRITTEN CODE INSTRUCTIONS 
+ 
+print ('hello')
+year = input ("what year is it ?")
+print ( year )
+
+# input () a built in function that allows a user to  
+# type in data into the terminal. the data that is passed 
+ # in will always be treated as a string data type. 
+  
+# print () a built in function that allows a user to 
+# print data out in terminal.  
+ 
+
+
+ # data casting functions
+# these are built in ( prewritten) functions 
+# that changes data types from one form into another  
+ 
+ # str () - this datacasting functions allows you to change 
+ # any data type into a string 
+  
+   
+year = 1906 
+print ( " this event took place in"+ str (year )) 
+
+# string concatenation - combining string data types 
+# with one another. we use plus sign (+) operator 
+# to combine strings together 
+ 
+  
+#int- Int, or integer, is a whole number, positive or negative, without decimals, of unlimited length
+ 
+#float-Float, or "floating point number" is a number, positive or negative, containing one or more decimals.
+
+
+
+
+
+
+
